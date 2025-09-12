7c65a4c8599a33ee78652361185e2d7d86dd9658
dtnehere <neheredhanashree05@gmail.com>
2025-09-12 16:37:08 +0530

    added MaheshLogout file

--- Mahesh/Homepage.java
+++ Mahesh/Homepage.java
@@ -1,5 +1,7 @@
+package Mahesh;
+
 public class Homepage {
     public static void main(String[] args) {
-        System.out.println("This is Homepage");
+        System.out.println("This is Mahesh.Homepage");
     }
 }
---
+++ Mahesh/MaheshLogout.java
@@ -0,0 +1,7 @@
+package Mahesh;
+
+public class MaheshLogout {
+    public static void main(String[] args) {
+        System.out.println("added logout file");
+    }
+}
