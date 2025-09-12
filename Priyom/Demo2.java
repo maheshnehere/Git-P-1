03aad76d68b4a5e325d3689175e1c044503edd2d
dtnehere <neheredhanashree05@gmail.com>
2025-09-12 17:04:53 +0530

    mahesh made changes to demo folder

--- Priyom/Demo2.java
+++ Priyom/Demo2.java
@@ -3,5 +3,6 @@ package Priyom;
 public class Demo2 {
     public static void main(String[] args) {
         System.out.println("Demo added");
+        System.out.println("mahesh changed demo");
     }
 }
